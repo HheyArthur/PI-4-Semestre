81f1983de349e21f850817c47e134061b15428fb
Angelo <92029180+Anbfer@users.noreply.github.com>
2024-03-06 15:06:36 -0300

    Alterações basicas À tela de login, implementação o script de login

--- index/tela/Login/login.js
+++ index/tela/Login/login.js
@@ -1,7 +1,36 @@
-function Openmodal() {
+  function loginUser() {
+    var email = document.getElementById("typeEmailX").value;
+    var password = document.getElementById("senha").value;
+
+    var user = {
+      email: email,
+      senha: senha
+    };
+
+  
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/usuarios/login",
+      data: JSON.stringify(user),
+      contentType: "application/json",
+      success: function (response) {
+        console.log("Login successful!" + response);
+      },
+      error: function (xhr, status, erro) {
+        console.lo("Erro: " + erro + " Status: " + status + " " + xhr.responseText);
+      }
+    });
+  }
+
+
+  function Openmodal() {
     document.getElementById("myPopup").style.display = "block";
   }
+
 
   function Closemodal() {
     document.getElementById("myPopup").style.display = "none";
   }
+
+
+  document.getElementById("loginBtn").addEventListener("click", loginUser);
