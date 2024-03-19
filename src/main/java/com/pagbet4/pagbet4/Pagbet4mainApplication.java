4269fe536de587f389836ee8e21ca5c5618e94c9
Angelo <92029180+Anbfer@users.noreply.github.com>
2024-03-19 14:04:33 -0300

    Atualização de log, apenas erros serão exibios ao iniciar

--- src/main/java/com/pagbet4/pagbet4/Pagbet4mainApplication.java
+++ src/main/java/com/pagbet4/pagbet4/Pagbet4mainApplication.java
@@ -1,15 +1,13 @@
 package com.pagbet4.pagbet4;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
 
 @SpringBootApplication
 public class Pagbet4mainApplication {
 
 	public static void main(String[] args) {
-		java.util.logging.Logger.getLogger("org.apache").setLevel(java.util.logging.Level.WARNING);
-		new SpringApplicationBuilder(Pagbet4mainApplication.class).logStartupInfo(false).run(args);
+		SpringApplication.run(Pagbet4mainApplication.class, args);
 	}
 
 }
